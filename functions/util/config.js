1f3e53874a25bac006549df60baafb0704a0a850
Niforances <niforances@gmail.com>
2022-05-10 15:30:15 +0900

    [Update] doorLockSystem boilerplate init

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyAjf4o6EC7mpgw_Ypd5bcfWrBgIibA0wEM",
+    authDomain: "sy-project-28c0d.firebaseapp.com",
+    databaseURL: "https://sy-project-28c0d-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "sy-project-28c0d",
+    storageBucket: "sy-project-28c0d.appspot.com",
+    messagingSenderId: "14128280016",
+    appId: "1:14128280016:web:76bcc039304ad30fa209d5",
+    measurementId: "G-MSJDQ9P208"
+  };
